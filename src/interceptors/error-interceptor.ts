6385e6c0e60ed846e8206c4900b8b5c63bd5b93f
Rodrigo arruda <arruda384digo@gmail.com>
2019-05-29 18:14:02 -0300

    erro padrão

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,12 +3,14 @@ import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { catchError } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
+import { Alert, AlertPromise } from 'selenium-webdriver';
  
  
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
  
-    constructor(public storage: StorageService){ }
+    constructor(public storage: StorageService, public alertCtrl: AlertController){ }
  
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         return next.handle(req)
@@ -18,8 +20,18 @@ export class ErrorInterceptor implements HttpInterceptor{
                             error = JSON.parse(error);
                         }
                         switch(error.status){
+                            
+                            case 401: this.handle401();
+                            break;
+
                             case 403: this.handle403();
                             break;
+
+                            default:
+                                this.handleDefaultError(error);
+
+
+
                         }
  
                         return throwError(error);
@@ -31,6 +43,42 @@ handle403(){
         this.storage.setLocalUser(null);
     }
  
+    async  handle401(){
+        const alert = await this.alertCtrl.create({
+            
+            
+            header: 'Erro 401: Falha de autenticação',
+            message: 'Email e/ou senha incorreto(s)',
+            backdropDismiss: false,
+           
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    async  handleDefaultError(error){
+        
+        const alert = await this.alertCtrl.create({
+            
+            header: 'Erro ' + error.status + ':' + error.error,
+            message: error.message,
+            backdropDismiss: false,
+           
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
 }
  
  
